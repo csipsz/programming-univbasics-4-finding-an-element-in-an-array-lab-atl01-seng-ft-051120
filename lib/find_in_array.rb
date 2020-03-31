17ab8bfcde37f9f63656b2fced7858b0962b3696
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 14:22:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  result = []
   while count < array.length do 
     if array[count] == value_to_find
-      puts count
+      result.push(count)
     end
     count += 1
   end
